6f35a19be416ff204e3c7aa76887bf3ebf8b043f
AthibanK <athiban2507@gmail.com>
2020-10-23 17:14:26 +0530

    the animation

--- javaScript/game.js
+++ javaScript/game.js
@@ -27,9 +27,22 @@ class Game{
         textSize(15);
         text("Who is the best racer ?",75,200);
         Player.getPlayerInfo();
-        var yPosition = 200;
+        //var yPosition = 200;
         if(allPlayers!== undefined){
+           var carIndex = 0;
+           var xPosition = displayWidth/8; //&&displayWidth/4/2;
+           var yPosition;
             for(var plyr in allPlayers){
+                carArray[carIndex].x = xPosition;
+                carArray[carIndex].y = height - allPlayers[plyr].distance;
+                carIndex+=1;
+                xPosition+=displayWidth/4;
+                if((carIndex+1)===player.position){
+                    carArray[carIndex].shapeColor = "red";
+                    camera.position.x = displayWidth/2;
+                    camera.position.y = carArray[carIndex].y
+                }
+                /*
                 if(plyr==="player"+player.position)
                     fill("red");
                 else
@@ -37,12 +50,14 @@ class Game{
                 textSize(14);
                 text(allPlayers[plyr].name +","+ allPlayers[plyr].distance,250,yPosition);
                 yPosition+=15;
+                */
             }
+
         }
         if(keyIsDown(UP_ARROW) && player.position!== null){
             player.distance+=20;
             player.update();
          }
-        
+        drawSprites();
     }
 }
--- sketch.js
+++ sketch.js
@@ -5,15 +5,22 @@ var game;
 var playerCount;
 var gameState;
 var allPlayers;
+var redCar,blueCar,blackCar,whiteCar;
+var carArray;
 
 
 function setup(){
   database = firebase.database(); 
-  createCanvas(500,500);
+  createCanvas(displayWidth,displayHeight);
  gameState = 0;
  game = new Game();
  game.getState();
  game.start();
+ redCar = createSprite(100,25,20,20);
+ blackCar = createSprite(175,40,20,20);
+ blueCar = createSprite(250,25,20,20);
+ whiteCar = createSprite(355,40,20,20);
+ carArray = [redCar,blueCar,blackCar,whiteCar]; 
 }
 
 function draw(){
